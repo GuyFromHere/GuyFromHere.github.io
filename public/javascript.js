4fff723273fcb1a4ed634c402a804caf70978b47
GuyFromHere <47902660+GuyFromHere@users.noreply.github.com>
2019-06-10 00:22:13 -0700

    added timer and lost modal, scores

--- public/javascript.js
+++ public/javascript.js
@@ -12,10 +12,11 @@ const cellIndex = [ 10, 20, 30 ]
 const mineCount = [ 12, 50, 112]; 
 var difficulty = 0;
 var cellDictionary = {};
-const debug = false;
-
+var timer; 
+var cellCount;
 var Game = new Object();
 
+// LOG & DEBUG FUNCTIONS
 function resetLog() {
     const logDiv = document.getElementById("log");
     logDiv.innerHTML = "";
@@ -26,42 +27,95 @@ function addLog(text) {
     logDiv.innerHTML += ('<p>' + text + '</p>');
 }
 
+// TIMER FUNCTIONS
+function pad(val) {
+    return val > 9 ? val : "0" + val;
+}
+
+function timer(sec) {
+    setInterval(function () {
+        document.getElementById("seconds").innerHTML = pad(++sec % 60);
+        document.getElementById("minutes").innerHTML = pad(parseInt(sec / 60, 10));
+    }, 1000);
+}
+
+function resetTimer () {
+    clearInterval(timer);
+}
+
+// EVENT HANDLERS
+document.addEventListener('click', function(e) {
+    //e = e || window.event;
+    var target = e.target || e.srcElement,
+    text = target.textContent || target.innerText;
+    if ( target.className == 'cell' ) {
+        sweep(target.id, Game.difficulty )
+    }
+}, false);
+
+/* document.addEventListener('contextmenu', function(e) {
+    //e = e || window.event;
+    var target = e.target || e.srcElement,
+    text = target.textContent || target.innerText;
+    addLog(target.className);
+    if ( target.className == 'flag-cell' ) {
+        target.className = 'mystery-cell';
+        target.innerHTML = '?';
+        changeScore(1);
+    } else if ( target.className == 'cell') {
+        target.className = 'flag-cell';
+        target.innerHTML = '<img src="public/img/flag.png" class="flag-png">'
+        changeScore(-1);
+    } else if ( target.className == 'mystery-cell' ) {
+        target.className = 'cell';
+        target.innerHTML = '';
+    }
+    e.preventDefault();
+}, false); */
+
+function changeScore(num) {
+    Game.mines += num;
+    document.getElementById('mines-remaining').innerHTML = Game.mines;
+}
+
+function bestTimes() {
+    const minutes = document.getElementById('minutes');
+    const seconds = document.getElementById('seconds');
+    var timeTable = document.getElementById('best-time');
+    var timeTableRow = document.createElement('tr');
+    var timeTableCell = document.createElement('td');
+    timeTableCell.innerHTML = (minutes.innerHTML + ':' + seconds.innerHTML);
+    timeTableRow.appendChild(timeTableCell);
+    timeTable.appendChild(timeTableRow);
+}
+
 function flag(id) {
     const cell = document.getElementById(id);
     if ( cell.className == 'flag-cell' ) {
         cell.className = 'mystery-cell';
         cell.innerHTML = '?';
+        changeScore(1);
     } else if ( cell.className == 'cell') {
         cell.className = 'flag-cell';
         cell.innerHTML = '<img src="public/img/flag.png" class="flag-png">'
+        changeScore(-1);
     } else if ( cell.className == 'mystery-cell' ) {
         cell.className = 'cell';
         cell.innerHTML = '';
     }
 }
 
-function safe(id) {
-    const element = document.getElementById(id);
-    element.className = 'clear'; 
-    element.innerHTML = cellDictionary[id].count;
-}
-
 function sweep(id, difficulty) {
     if ( cellDictionary[id].isMine) {
-        var element = document.getElementById(id);
+        resetTimer();
         var gameOver = document.getElementById('game-over');
-        //addLog('show modal');
         gameOver.showModal();
-        element.className = 'boom';
-        element.innerHTML = "X";
+        var cell = document.getElementById(id);
+        cell.className = 'boom';
+        cell.innerHTML = "X";
     } else {
         checkSurroundingCells(id, difficulty);
     }  
-}
-
-function clear(id) {
-    const element = document.getElementById(id);
-    element.className = 'clear';
 }
 
 function getRandomNumber(num) {
@@ -70,17 +124,13 @@ function getRandomNumber(num) {
 }
 
 function seedMines(difficulty) {
-    var mineCounter = 0;
+    var mineCounter = 1;
     do {
         var randomX = getRandomNumber(cellIndex[difficulty]); 
         var randomY = getRandomNumber(cellIndex[difficulty]);
         if ( !cellDictionary[randomX + ' ' + randomY].isMine ) {
             cellDictionary[randomX + ' ' + randomY].isMine = true;
             mineCounter++;
-            if (debug) {
-              let cell = document.getElementById(randomX + ' ' + randomY);
-              cell.innerHTML = "X";
-            }
         }
     }
     while (mineCounter <= mineCount[difficulty] );
@@ -91,15 +141,16 @@ function checkSurroundingCells(id, difficulty) {
     if ( cell.innerHTML == "" || cell.innerHTML == "?" ) {
         for (var x = Math.max(cellDictionary[id].posX-1,0);x<=Math.min(cellDictionary[id].posX+1,(cellIndex[difficulty]-1));x++) {
             for (var y = Math.max(cellDictionary[id].posY-1,0);y<=Math.min(cellDictionary[id].posY+1,(cellIndex[difficulty]-1));y++) {
-                if ( cellDictionary[x + ' ' + y].isMine) {
-                    cellDictionary[id].count++;
-                }
+                if ( cellDictionary[x + ' ' + y].isMine) { cellDictionary[id].count++; }
             }
         }
         if (cellDictionary[id].count > 0) {
-            safe(id);
+            document.getElementById(id).className = 'clear';
+            document.getElementById(id).innerHTML = cellDictionary[id].count;
+            Game.remainingCells -= 1;
         } else {
-            clear(id);
+            document.getElementById(id).className = 'clear';
+            Game.remainingCells -= 1;
             for (var x = Math.max(cellDictionary[id].posX-1,0);x <=Math.min(cellDictionary[id].posX+1,(cellIndex[difficulty]-1));x++) {
                 for (var y = Math.max(cellDictionary[id].posY-1,0);y <= Math.min(cellDictionary[id].posY+1,(cellIndex[difficulty]-1));y++) {
                     if (document.getElementById(x + ' ' + y).className == 'cell') {
@@ -109,31 +160,45 @@ function checkSurroundingCells(id, difficulty) {
             }
         }
     }
+    if ( Game.remainingCells == 12 && Game.mines == 0 ) {
+        var winner = document.getElementById('you-win');
+        alert('there');
+        resetTimer();
+        bestTimes();
+        winner.showModal();
+    }
 }
 
 function buildBoard() {
     resetLog();
+    resetTimer();
+    var sec = 0;
+    Game.difficulty = getDifficulty();
+    Game.mines = mineCount[Game.difficulty];
+    Game.remainingCells = cellIndex[Game.difficulty] * cellIndex[Game.difficulty];
+    document.getElementById('mines-remaining').innerHTML = Game.mines;
+
     var board = document.getElementById('board');
     board.innerHTML = "";
     board.className = "board";
-    var difficulty = getDifficulty();
+    //board.setAttribute('oncontextmenu', 'javascript:selectCell();return false;');
     
-    if (!difficulty || difficulty == 0) {
-            var difficulty = 0;
+    if (!Game.difficulty || Game.difficulty == 0) {
+            //var difficulty = 0;
             board.classList.add('easy');
-        } else if (difficulty == 1) {
+        } else if (Game.difficulty == 1) {
             board.classList.add('moderate');
-        } else if (difficulty == 2) {
+        } else if (Game.difficulty == 2) {
             board.classList.add('hard');
     }
-    for ( var x = 0; x < cellIndex[difficulty]; x++) {
-        for ( var y = 0; y < cellIndex[difficulty]; y++) {
+
+    for ( var x = 0; x < cellIndex[Game.difficulty]; x++) {
+        for ( var y = 0; y < cellIndex[Game.difficulty]; y++) {
             var newNode = document.createElement('div');
             newNode.className = 'cell';
             newNode.id = (x + ' ' + y);
             newNode.innerHTML = "";
             newNode.setAttribute('oncontextmenu','javascript:flag(this.id);return false;');
-            newNode.setAttribute('onclick','javascript:sweep(this.id, ' + difficulty + ');');
             let newCell = {
                 id: newNode.id,
                 posX: x,
@@ -145,6 +210,11 @@ function buildBoard() {
             board.appendChild(newNode);
         }
     }
-    seedMines(difficulty);
-}
+    seedMines(Game.difficulty);
+    
+    timer = setInterval(function () {
+        document.getElementById("seconds").innerHTML = pad(++sec % 60);
+        document.getElementById("minutes").innerHTML = pad(parseInt(sec / 60, 10));
+    }, 1000);
 
+}
